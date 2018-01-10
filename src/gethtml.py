eafa5a3de3fd1717fba5d9ccbab3fa661126c1b4
bomch4nte <bomch4nte@gmail.com>
2018-01-10 21:39:27 +0300

    Update gethtml.py

--- src/gethtml.py
+++ src/gethtml.py
@@ -2,7 +2,9 @@
 #-*- coding: utf-8 -*-
 #
 #
+
 import urllib2
+
 def getHTML(url, data=None, user_agent=None, timeout=3):
   request = urllib2.Request(url, data=data, headers={'User-Agent': user_agent})
   return urllib2.urlopen(request, timeout=timeout)
